a117e05f5ee408ecc0c205fed64eeb48b2a4533f
ldangelo1 <ldangelo1@studenti.uninsubria.it>
2022-08-19 21:11:35 +0200

    create objects

---
+++ src/main/java/uni/emotionalsongs/objects/Address.java
@@ -0,0 +1,49 @@
+package uni.emotionalsongs.objects;
+
+public class Address {
+    private final String qualifier;
+    private final String addrName;
+    private final Integer civicNum;
+    private final String city;
+    private final String province;
+    private final Integer cap;
+
+    Address(String qualifier, String addrName, Integer civicNum, String city, String province, Integer cap) {
+        this.qualifier = qualifier;
+        this.addrName = addrName;
+        this.civicNum = civicNum;
+        this.city = city;
+        this.province = province;
+        this.cap = cap;
+    }
+
+    @Override
+    public String toString() {
+        return qualifier + " " + addrName + " " + civicNum + ", " + city + "(" + province + ")" + cap;
+    }
+
+    /// Getters
+    public String getQualifier() {
+        return qualifier;
+    }
+
+    public String getAddrName() {
+        return addrName;
+    }
+
+    public Integer getCivicNum() {
+        return civicNum;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public Integer getCap() {
+        return cap;
+    }
+}
---
+++ src/main/java/uni/emotionalsongs/objects/Song.java
@@ -0,0 +1,10 @@
+package uni.emotionalsongs.objects;
+
+public class Song {
+    private String Title;
+    private String Autore;
+    private String Album;
+    private Integer Year;
+    private String Genre;
+    private Integer Duration;
+}
