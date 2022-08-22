362ee8f475ecccfe43f6f7943aa1f95300a91c48
ldangelo1 <ldangelo1@studenti.uninsubria.it>
2022-08-22 18:49:22 +0200

    Make no resizable the window

--- src/main/java/uni/emotionalsongs/search/Search.java
+++ src/main/java/uni/emotionalsongs/search/Search.java
@@ -17,6 +17,7 @@ public class Search extends Application {
 		FXMLLoader fxmlLoader = new FXMLLoader(Search.class.getResource("search-view.fxml"));
 		Scene scene = new Scene(fxmlLoader.load(), 800, 600);
 		stage.setTitle("Search");
+		stage.setResizable(false);
 		stage.setScene(scene);
 		stage.show();
 	}
