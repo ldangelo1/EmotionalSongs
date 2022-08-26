90b056b35bff7f6afc677c30ea48b785a279dcbc
ldangelo1 <ldangelo1@studenti.uninsubria.it>
2022-08-26 18:14:46 +0200

    auto-update module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,17 +1,18 @@
 module uni.emotionalsong {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-    requires java.sql;
-    requires org.controlsfx.controls;
-    requires org.kordamp.bootstrapfx.core;
-
-    opens uni.emotionalsongs.search to javafx.fxml;
-    opens uni.emotionalsongs.signin to javafx.fxml;
-    opens uni.emotionalsongs.signup to javafx.fxml;
-
-    exports uni.emotionalsongs.search;
-    exports uni.emotionalsongs.signin;
-    exports uni.emotionalsongs.signup;
+	requires javafx.controls;
+	requires javafx.fxml;
+	
+	requires com.dlsc.formsfx;
+	requires java.sql;
+	requires org.controlsfx.controls;
+	requires org.kordamp.bootstrapfx.core;
+	
+	opens uni.emotionalsongs to javafx.fxml;
+	
+	opens uni.emotionalsongs.signin to javafx.fxml;
+	opens uni.emotionalsongs.signup to javafx.fxml;
+	
+	exports uni.emotionalsongs;
+	exports uni.emotionalsongs.signin;
+	exports uni.emotionalsongs.signup;
 }
