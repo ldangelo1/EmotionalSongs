a45652a7e1270cedfaccbf6858710e85e19f84e5
ldange00 <dng.lorenzo.00@gmail.com>
2022-10-30 00:39:24 +0200

    aggiornato la var bool per accesso al db, nel caso doveste usarla voi

--- uni/java/database/DBInfo.java
+++ uni/java/database/DBInfo.java
@@ -1,10 +1,9 @@
 package database;
 
-
 public class DBInfo {
 	// TODO: 29/10/22 Ho settato il mio db con diverse credenziali:
 	// la variabile le gestisce, se true è il vostro db.
-	private static final Boolean dbDefault = false;
+	private static final Boolean dbDefault = true;
 	
 	public static String getIP() {
 		return "localhost";
