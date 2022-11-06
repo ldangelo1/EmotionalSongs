d0c1b1c0d09b35d0f7fd25f3ab66b35c0528879f
ldange00 <dng.lorenzo.00@gmail.com>
2022-11-06 23:52:26 +0100

    Inutile come classe, diventa record (o struct del C)

--- uni/java/emotionalsongs/objects/Indirizzo.java
+++ uni/java/emotionalsongs/objects/Indirizzo.java
@@ -1,49 +1,8 @@
 package emotionalsongs.objects;
 
-public class Indirizzo {
-	private final String qualifier;
-	private final String addrName;
-	private final Integer civicNum;
-	private final String city;
-	private final String province;
-	private final Integer cap;
-	
-	Indirizzo(String qualifier, String addrName, Integer civicNum, String city, String province, Integer cap) {
-		this.qualifier = qualifier;
-		this.addrName = addrName;
-		this.civicNum = civicNum;
-		this.city = city;
-		this.province = province;
-		this.cap = cap;
-	}
-	
+public record Indirizzo(String qualif, String addr, Integer civic, String city, String prov, Integer cap) {
 	@Override
 	public String toString() {
-		return qualifier + " " + addrName + " " + civicNum + ", " + city + "(" + province + ")" + cap;
-	}
-	
-	/// Getters
-	public String getQualifier() {
-		return qualifier;
-	}
-	
-	public String getAddrName() {
-		return addrName;
-	}
-	
-	public Integer getCivicNum() {
-		return civicNum;
-	}
-	
-	public String getCity() {
-		return city;
-	}
-	
-	public String getProvince() {
-		return province;
-	}
-	
-	public Integer getCap() {
-		return cap;
+		return qualif + " " + addr + " " + civic + ", " + city + " (" + prov + ") " + cap;
 	}
 }
