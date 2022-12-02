429a208b58601c27849f40bd8b738eaa0d548fc6
ldange00 <dng.lorenzo.00@gmail.com>
2022-12-02 02:12:56 +0100

    tolto moduli non necessari

--- uni/java/module-info.java
+++ uni/java/module-info.java
@@ -1,12 +1,7 @@
 module uni.emotionalsong {
 	requires javafx.controls;
 	requires javafx.fxml;
-	
-	requires com.dlsc.formsfx;
 	requires java.sql;
-	requires org.controlsfx.controls;
-	requires org.kordamp.bootstrapfx.core;
-	requires org.postgresql.jdbc;
 	
 	opens emotionalsongs to javafx.fxml;
 	
