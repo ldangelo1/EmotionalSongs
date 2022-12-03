71ac1e63eeb9040a308e579429a0b9313de1b8b9
ldange00 <dng.lorenzo.00@gmail.com>
2022-12-04 00:41:55 +0100

    aggiunto requires in module-info.java

--- uni/java/module-info.java
+++ uni/java/module-info.java
@@ -2,6 +2,7 @@ module uni.emotionalsong {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires java.sql;
+	requires org.postgresql.jdbc;
 	
 	opens emotionalsongs to javafx.fxml;
 	opens emotionalsongs.objects to javafx.fxml;
