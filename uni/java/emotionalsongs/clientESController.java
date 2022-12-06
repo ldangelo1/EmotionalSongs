98f0c8cfa81cd65ec3d85212e2771bd721669306
ldange00 <dng.lorenzo.00@gmail.com>
2022-12-06 02:08:17 +0100

    inizializzato le tabelle e la ricerca sulle canzoni è finita. Rimossa la colonna Album, rinominato Autore -> Artista Aggiunto la chiamata alla finestra di recensione

--- uni/java/emotionalsongs/clientESController.java
+++ uni/java/emotionalsongs/clientESController.java
@@ -5,6 +5,7 @@ import database.serverES;
 import emotionalsongs.account.Account;
 import emotionalsongs.objects.Canzone;
 import emotionalsongs.objects.Playlist;
+import emotionalsongs.recensione.Recensione;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -17,23 +18,24 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class clientESController {
-	private final String[] ricercaStrings = {"Titolo", "Autore", "Anno", "Autore e Anno"};
-	private final String[] emozioneStrings = {""};
+	private final String[] ricercaStrings = {"Titolo", "Artista", "Anno", "Artista e Anno"};
 	private Boolean isLogged;
 	
 	@FXML
 	private Label userLbl, avvisoLbl;
 	@FXML
-	private TextField titoloField, autoreField, annoField;
+	private TextField titoloField, artistaField, annoField, addPListField;
 	
 	@FXML
 	private TableView<Canzone> canzoneTable, plistCanzoneTable;
 	@FXML
 	private TableView<Playlist> plistTable;
 	@FXML
-	private TableColumn<Canzone, Integer> colAnno;
+	private TableColumn<Canzone, Integer> colAnno_Canzone, colAnno_PList;
 	@FXML
-	private TableColumn<Canzone, String> colTitolo, colAutore, colAlbum, colEmozione;
+	private TableColumn<Canzone, String> colTitolo_Canzone, colArtista_Canzone, colTitolo_PList, colArtista_PList;
+	@FXML
+	private TableColumn<Playlist, String> colPList;
 	
 	@FXML
 	private ChoiceBox<String> ricercaCBox;
@@ -48,9 +50,15 @@ public class clientESController {
 	}
 	
 	@FXML
+	private void recensione() throws IOException {
+		Stage stage = new Stage();
+		new Recensione().start(stage);
+	}
+	
+	@FXML
 	private void song() throws Exception {
 		String titolo = titoloField.getText();
-		String autore = autoreField.getText();
+		String artista = artistaField.getText();
 		String anno = annoField.getText();
 		String ricerca = ricercaCBox.getValue();
 		String query = "SELECT * FROM \"Canzone\" ";
@@ -59,45 +67,22 @@ public class clientESController {
 			case "Titolo" -> {
 				if (!titolo.isEmpty()) query += "WHERE \"Titolo\"=" + "'" + titolo + "'";
 			}
-			case "Autore" -> {
-				if (!autore.isEmpty()) query += "WHERE \"Autore\"=" + "'" + autore + "'";
+			case "Artista" -> {
+				if (!artista.isEmpty()) query += "WHERE \"Artista\"=" + "'" + artista + "'";
 			}
 			case "Anno" -> {
 				if (!anno.isEmpty()) query += "WHERE \"Anno\"=" + anno;
 			}
-			case "Autore e Anno" -> {
-				if (!autore.isEmpty() && !anno.isEmpty())
-					query += "WHERE \"Autore\"=" + "'" + autore + "' " + "AND \"Anno\"=" + anno;
+			case "Artista e Anno" -> {
+				if (!artista.isEmpty() && !anno.isEmpty())
+					query += "WHERE \"Artista\"=" + "'" + artista + "' " + "AND \"Anno\"=" + anno;
 			}
 		}
 		
 		ResultSet rset = (ResultSet) serverES.eseguiQuery(query, 1);
 		assert rset != null;
-		printCanzoneTable(rset);
+		canzoneTable.setItems(queryCanzone(rset));
 		rset.close();
-	}
-	
-	/**
-	 * Sfoglio canzone per canzone aggiungendole in lista,
-	 * successivamente popolo canzoneTable con la lista.
-	 *
-	 * @param rset risultato della query
-	 * @throws SQLException rset.next() genera l'eccezione
-	 */
-	private void printCanzoneTable(ResultSet rset) throws SQLException {
-		ObservableList<Canzone> data = FXCollections.observableArrayList();
-		
-		while (rset.next()) {
-			Canzone canzone = new Canzone();
-			canzone.setAnno(rset.getInt("Anno"));
-			canzone.setID(rset.getString("ID"));
-			canzone.setAutore(rset.getString("Autore"));
-			canzone.setTitolo(rset.getString("Titolo"));
-			canzone.setAlbum(rset.getString("Album"));
-			data.add(canzone);
-		}
-		
-		canzoneTable.setItems(data);
 	}
 	
 	/**
@@ -134,15 +119,48 @@ public class clientESController {
 		ricercaCBox.getItems().addAll(ricercaStrings);
 		ricercaCBox.setValue(ricercaStrings[0]);
 		
-		assert canzoneTable != null;
-		colAnno.setCellValueFactory(new PropertyValueFactory<>("Anno"));
-		colEmozione.setCellValueFactory(new PropertyValueFactory<>("ID"));
-		colAutore.setCellValueFactory(new PropertyValueFactory<>("Autore"));
-		colTitolo.setCellValueFactory(new PropertyValueFactory<>("Titolo"));
-		colAlbum.setCellValueFactory(new PropertyValueFactory<>("Album"));
+		initTableView(canzoneTable, colTitolo_Canzone, colArtista_Canzone, colAnno_Canzone);
+		initTableView(plistCanzoneTable, colTitolo_PList, colArtista_PList, colAnno_PList);
+		initPListTable(plistTable, colPList);
 		
 		userLbl.setText("Necessita di account per sbloccare altre funzioni");
 	}
+	
+	private void initTableView(TableView<Canzone> Table, TableColumn<Canzone, String> colTitolo, TableColumn<Canzone, String> colArtista, TableColumn<Canzone, Integer> colAnno) {
+		assert Table != null;
+		colTitolo.setCellValueFactory(new PropertyValueFactory<>("Titolo"));
+		colArtista.setCellValueFactory(new PropertyValueFactory<>("Artista"));
+		colAnno.setCellValueFactory(new PropertyValueFactory<>("Anno"));
+	}
+	
+	private void initPListTable(TableView<Playlist> Table, TableColumn<Playlist, String> colPList) {
+		assert Table != null;
+		colPList.setCellValueFactory(new PropertyValueFactory<>("Playlist"));
+	}
+	
+	/**
+	 * Sfoglio canzone per canzone aggiungendole in lista,
+	 * successivamente popolo la tabella con la lista.
+	 *
+	 * @param rset risultato della query
+	 * @throws SQLException rset.next() genera l'eccezione
+	 */
+	private ObservableList<Canzone> queryCanzone(ResultSet rset) throws SQLException {
+		ObservableList<Canzone> data = FXCollections.observableArrayList();
+		
+		while (rset.next()) {
+			Canzone canzone = new Canzone();
+			canzone.setTitolo(rset.getString("Titolo"));
+			canzone.setArtista(rset.getString("Artista"));
+			canzone.setAnno(rset.getInt("Anno"));
+			data.add(canzone);
+		}
+		return data;
+	}
+	
+	// TODO: 04/12/22 queryCanzone ora è atomicizzato(? esiste il termine), da fare per le playlist
+	// ho eliminato le emozioni dalla tabella, quando si preme su una canzone si apre una nuova finestra dove
+	// si vedono le "recensioni" e, se loggato, puoi recensire
 	
 	public Boolean getLogged() {
 		return isLogged;
