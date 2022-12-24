ac41fb87d8f1c0ac95d239f11c967be49e6578c4
ldange00 <dng.lorenzo.00@gmail.com>
2022-12-24 20:07:27 +0100

    corretto la tabella Contiene.sql, rieseguire lo script

--- uni/resources/database/tables/Contiene.sql
+++ uni/resources/database/tables/Contiene.sql
@@ -2,7 +2,7 @@ drop table if exists "Contiene" cascade;
 
 create table "Contiene"
 (
-    "fk_Playlist" integer references "Playlist",
+    "fk_Playlist" integer references "Playlist" on delete cascade on update cascade,
     "ID"          varchar(18) references "Canzone" on delete cascade on update cascade
 );
 
