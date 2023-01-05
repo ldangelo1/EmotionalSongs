7a3f0d0808a26061eef20ffdfb845b31d11dfbed
ldange00 <dng.lorenzo.00@gmail.com>
2023-01-05 21:43:07 +0100

    completata la funzione per aggiungere o aggiornare le emozioni

--- uni/java/emotionalsongs/recensione/RecensioneController.java
+++ uni/java/emotionalsongs/recensione/RecensioneController.java
@@ -1,32 +1,74 @@
 package emotionalsongs.recensione;
 
+import database.serverES;
+import emotionalsongs.clientESController;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextField;
+import javafx.scene.Node;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 
-public class RecensioneController {
-	private final Integer[] votoInt = {1, 2, 3, 4, 5};
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class RecensioneController extends InfoController {
+	private final Integer[] votoInt = {0, 1, 2, 3, 4, 5};
 	@FXML
 	private AnchorPane sceneAccount;
 	
-	@FXML
-	private ChoiceBox<Integer> stupCBox, soleCBox, teneCBox, nostCBox, calmCBox, poteCBox, gioiCBox, tensCBox, trisCBox;
-	@FXML
-	private TextField stupField, soleField, teneField, nostField, calmField, poteField, gioiField, tensField, trisField;
-	
-	@FXML
-	private Button addEmozioneBtn;
-	
 	public void initialize() {
-		for (Object cbox : sceneAccount.getChildren()) {
-			if (cbox instanceof ChoiceBox<?>) initCBox((ChoiceBox<Integer>) cbox);
-		}
+		sceneAccount.getChildren().stream().filter(cbox -> cbox instanceof ChoiceBox<?>).map(cbox -> (ChoiceBox<Integer>) cbox).forEachOrdered(this::initCBox);
 	}
 	
 	private void initCBox(ChoiceBox<Integer> cbox) {
 		cbox.getItems().addAll(votoInt);
 		cbox.setValue(votoInt[0]);
+	}
+	
+	public void addEmozione() throws SQLException {
+		if (estraiValori() > 0)
+			serverES.generaAlert(Alert.AlertType.INFORMATION, "Emozioni aggiunte con successo.", "Premi OK per tornare.");
+		serverES.chiudiStage(sceneAccount);
+	}
+	
+	private Integer estraiValori() throws SQLException {
+		boolean jump = true;
+		Integer count = 0;
+		String queryTail;
+		String queryCond;
+		
+		String nomeEmozione = "";
+		Integer score = 0;
+		String note;
+		
+		for (Node node : sceneAccount.getChildren()) {
+			if (node instanceof Button) continue;
+			if (node instanceof Label) {
+				nomeEmozione = ((Label) node).getText().replaceAll("([\\wà]):.*", "$1");
+			}
+			if (node instanceof ChoiceBox<?>) {
+				score = (Integer) ((ChoiceBox<?>) node).getValue();
+				jump = score == 0;
+			}
+			if (node instanceof TextField && !jump) {
+				note = ((TextField) node).getText();
+				
+				if (checkEmozione(nomeEmozione)) {
+					queryTail = "VALUES('" + nomeEmozione + "', '" + clientESController.getCF() + "', '" + getCanzone().getID() + "', " + score + ", '" + note + "')";
+					if (serverES.insert("Emozione", queryTail) == 1) count++;
+				} else {
+					queryTail = "\"Valutazione\"=" + score + ", \"Note\"='" + note + "'";
+					queryCond = "\"Tipo\"='" + nomeEmozione + "' AND \"CF\"='" + clientESController.getCF() + "' AND \"ID\"='" + getCanzone().getID() + "'";
+					if (serverES.update("Emozione", queryTail, queryCond) == 1) count++;
+				}
+			}
+		}
+		return count;
+	}
+	
+	private Boolean checkEmozione(String nomeEmozione) throws SQLException {
+		ResultSet rset = serverES.select("Emozione", "WHERE \"Tipo\"='" + nomeEmozione + "' AND \"CF\"='" + clientESController.getCF() +
+				"' AND \"ID\"='" + getCanzone().getID() + "'");
+		rset.next();
+		return rset.getRow() == 0;
 	}
 }
